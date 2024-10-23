80df26f8690c1a29ae8225224979f4969eff0d8a
Eros Borba <67812271+erosborba@users.noreply.github.com>
2024-10-22 22:06:54 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,6 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDoHFl67NVy8vpwExVz65S-zbAvGMtJ-iM",
-    authDomain: "imob-b968a.firebaseapp.com",
-    projectId: "imob-b968a",
-    storageBucket: "imob-b968a.appspot.com",
-    messagingSenderId: "513581880954",
-    appId: "1:513581880954:web:4fbed224a068766e83cdc3",
-    measurementId: "G-HJ6ENQ5EN4"
   };
 
 const app = initializeApp(firebaseConfig);
